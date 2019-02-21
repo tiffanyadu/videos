646eb8962ccb9e3656d9986a3c911edf443c2b9b
tiffanyadu <tadofdesign@gmail.com>
2019-02-20 22:05:25 -0600

    Set results data in state

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,17 @@ import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 
 class App extends React.Component {
+  state = {
+    videos: []
+  };
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
       params: {
         q: term
       }
     });
-    console.log(response);
+    this.setState({ videos: response.data.items });
+    console.log(response.data.items);
   };
   render() {
     return (

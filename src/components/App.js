8ec97ad160c90b9e4bba7b6c617ad10411698c7f
tiffanyadu <tadofdesign@gmail.com>
2019-02-20 22:01:05 -0600

    Set async/await terms

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,13 @@ import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 
 class App extends React.Component {
-  onTermSubmit = term => {
-    youtube.get("/search", {
+  onTermSubmit = async term => {
+    const response = await youtube.get("/search", {
       params: {
         q: term
       }
     });
+    console.log(response);
   };
   render() {
     return (

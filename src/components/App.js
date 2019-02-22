a86a5f96b16c00857719953b350d68094b8595ae
tiffanyadu <tadofdesign@gmail.com>
2019-02-21 21:33:54 -0600

    Render selected video description to page and styled it a bit

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 import youtube from "../apis/youtube";
 
 class App extends React.Component {
@@ -14,7 +15,6 @@ class App extends React.Component {
       }
     });
     this.setState({ videos: response.data.items, selectedVideo: null });
-    console.log(response.data.items);
   };
   onVideoSelect = video => {
     this.setState({ selectedVideo: video });
@@ -24,6 +24,7 @@ class App extends React.Component {
       <div className="ui container">
         <h1>App</h1>
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={this.onVideoSelect}
           videos={this.state.videos}

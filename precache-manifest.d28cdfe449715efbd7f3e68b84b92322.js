68ec512a15427a8e9f615b1bac63568d8ef873dc
tiffanyadu <tadofdesign@gmail.com>
2019-02-22 20:31:46 -0600

    Updates

---
+++ precache-manifest.d28cdfe449715efbd7f3e68b84b92322.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5911f0d1e63988ced4e1",
+    "url": "/videos/static/css/main.298dc6aa.chunk.css"
+  },
+  {
+    "revision": "5911f0d1e63988ced4e1",
+    "url": "/videos/static/js/main.5911f0d1.chunk.js"
+  },
+  {
+    "revision": "ea270d65a39a765d2854",
+    "url": "/videos/static/js/runtime~main.ea270d65.js"
+  },
+  {
+    "revision": "e250b45cfb4563bf5407",
+    "url": "/videos/static/js/2.e250b45c.chunk.js"
+  },
+  {
+    "revision": "c8c5d82299660b2776836f7ef2b5aae9",
+    "url": "/videos/index.html"
+  }
+];
